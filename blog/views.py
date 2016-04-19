ee61c89da5e7c763d752617938efd195ea2e62e1
hb <houbo_1020@163.com>
2016-04-19 23:05:01 +0800

    首页和文章详细的基本功能

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from django.shortcuts import render,get_object_or_404
+
+from .models import Article
 
 # Create your views here.
+
+def index(request):
+    latest_article_list = Article.objects.order_by('update')[:5]
+    context = {'latest_article_list':  latest_article_list}
+    return render(request, 'blog/index.html',context)
+
+def detail(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    return render(request, 'blog/p/detail.html', {'article': article})
