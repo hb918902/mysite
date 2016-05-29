eaf7f9b87aa8146c7c39edd378bc5412ef880e42
hb <houbo_1020@163.com>
2016-05-29 09:33:05 +0800

    1 重新布置模板文件位置  2 添加文章分类统计，使用模型聚合  3 添加时期分类

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^p/(?P<article_id>[0-9]+)/$', views.detail,name='detail'),
+    url(r'^main/(?P<article_id>[0-9]+)/$', views.detail,name='detail'),
     url(r'^register/$',views.register, name='register'),
     url(r'^login/$',views.my_login, name='my_login'),
     url(r'^logout/$',views.my_logout, name='my_logout'),
@@ -13,7 +13,7 @@ urlpatterns = [
     # Example: /2012/08/
     url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]+)/$',
         views.ArticleMonthArchiveView.as_view(month_format='%m'),
-        name="archive_month_numeric"),
+        name="index_by_month"),
 
 
 
--- blog/views.py
+++ blog/views.py
@@ -11,7 +11,27 @@ from django.views.generic.dates import MonthArchiveView
 
 from .models import Article, Category
 
+from django.db.models import Count
+
 # Create your views here.
+
+# 用于侧边栏月份文章归档统计
+def month_list():
+    articles = Article.objects.all()
+    year_month = set()
+    for a in articles:
+        year_month.add((a.cre_date.year,a.cre_date.month))
+    counter = {}.fromkeys(year_month,0)
+    for a in articles:
+        counter[(a.cre_date.year,a.cre_date.month)]+=1
+    year_month_number = []
+    for key in counter:
+        year_month_number.append([key[0],key[1],counter[key]])
+    year_month_number.sort(reverse=True)
+    return {'year_month_number':year_month_number}
+
+
+
 
 # blog首页
 def index(request):
@@ -20,36 +40,37 @@ def index(request):
     # return render(request, 'blog/index.html',context)
     # 重写完成分页功能
     article_list = Article.objects.all().order_by('cre_date')
-    paginator = Paginator(article_list,4) #show 4 articles per page
+    paginator = Paginator(article_list, 4)  # show 4 articles per page
 
     page = request.GET.get('page')
 
     try:
         articles = paginator.page(page)
     except PageNotAnInteger:
-        #页码不是整数，返回第一页。
+        # 页码不是整数，返回第一页。
         articles = paginator.page(1)
     except EmptyPage:
         articles = paginator.page(paginator.num_pages)
 
     d_key = {"articles": articles,
     "is_category": False,
-    "categories": Category.objects.all()}
+    "categories": Category.objects.annotate(num_article=Count('article'))}
 
+    d_key.update(month_list())  # 合并月份
 
-    # return render(request, 'blog/index.html', {'articles': articles})
-    return render(request, 'blog/index.html', d_key)
+    return render(request, 'blog/main/index.html', d_key)
 
 # 文章详细
-def detail(request, article_id):
+def detail(request,article_id):
     article = get_object_or_404(Article, pk=article_id)
 
     d_key = {"article": article,
     "is_category": False,
-    "categories": Category.objects.all()}
+    "categories": Category.objects.annotate(num_article = Count('article'))}
 
-    # return render(request, 'blog/p/detail.html', {'article': article})
-    return render(request, 'blog/p/detail.html', d_key)
+    d_key.update(month_list())
+
+    return render(request, 'blog/main/detail.html', d_key)
 
 
 # 用户注册
@@ -151,16 +172,28 @@ def category(request, pk):
     except EmptyPage:
         articles = paginator.page(paginator.num_pages)
 
-    return render(request,'blog/index.html',
-    {"article_list":article_list,
+    d_key = {"article_list":article_list,
     "articles": articles,
     "is_category": True,
     "cate_name": cate.name,
-    "categories": Category.objects.all()})
+    "categories": Category.objects.annotate(num_article = Count('article'))}
+
+    d_key.update(month_list())
+
+    return render(request, 'blog/main/index.html', d_key)
 
 
+
+# 月份归档
 class ArticleMonthArchiveView(MonthArchiveView):
+    template_name = 'blog/main/index_by_month.html'
     queryset = Article.objects.all()
     date_field = "cre_date"
-    allow_future = True
+    paginate_by = 4
+
+    def get_context_data(self, **kwargs):
+        context = super(ArticleMonthArchiveView,self).get_context_data(**kwargs)
+        context["categories"] = Category.objects.annotate(num_article = Count('article'))
+        context.update(month_list())
+        return context
 
